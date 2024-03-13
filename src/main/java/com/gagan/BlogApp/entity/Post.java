17b50f2b9ee763b08f5262d9c9568a012a3ea5fa
Gagan Randhawa <gagan23randhawa@gmail.com>
2024-03-13 21:32:28 +0530

    added User and post entity

--- src/main/java/com/gagan/BlogApp/entity/Post.java
+++ src/main/java/com/gagan/BlogApp/entity/Post.java
@@ -1,4 +1,124 @@
 package com.gagan.BlogApp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="title")
+    private String title;
+    @Column(name="excerpt")
+    private String excerpt;
+
+    @Column(name="content")
+    private String content;
+
+    @ManyToOne()
+    @Column(name="author_id")
+    private User author;
+
+    @Column(name="published_at")
+    private String publishedAt;
+
+    @Column(name="is_published")
+    private String isPublished;
+
+    @Column(name="created_at")
+    private String createdAt;
+
+    @Column(name="updated_at")
+    private String updatedAt;
+
+    public Post() {}
+
+    public Post(String title, String content, User author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getExcerpt() {
+        return excerpt;
+    }
+
+    public void setExcerpt(String excerpt) {
+        this.excerpt = excerpt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public String getIsPublished() {
+        return isPublished;
+    }
+
+    public void setIsPublished(String isPublished) {
+        this.isPublished = isPublished;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/gagan/BlogApp/entity/User.java
+++ src/main/java/com/gagan/BlogApp/entity/User.java
@@ -1,4 +1,30 @@
 package com.gagan.BlogApp.entity;
 
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.List;
+
+@Entity
+@Table(name="User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "enabled")
+    private boolean enabled = true;
+
+    @OneToMany(cascade=CascadeType.ALL, mappedBy = "user")
+    private List<Post> posts;
+
+
+
 }
